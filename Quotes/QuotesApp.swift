e041cca758249c68697bbb8375be955359eb1e62
Konneki <konnekdaniel@gmail.com>
2024-10-17 19:56:34 +0200

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Daniel Konnek on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
